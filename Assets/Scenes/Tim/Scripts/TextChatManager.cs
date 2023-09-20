32bd6ea0e5541b19024870f6600893928a0b83ee
tjwombo <tj.itliong@gmail.com>
2023-09-20 13:54:12 -0500

    Created initial folders/scripts

---
+++ Assets/Scenes/Tim/Scripts/TextChatManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GameFramework.Core.Data;
+
+public class TextChatManager : MonoBehaviour
+{
+    private GameFramework.Core.Data.LobbyPlayerData m;
+    private List<GameFramework.Core.Data.LobbyPlayerData> _lobbyPlayerDatas = new List<GameFramework.Core.Data.LobbyPlayerData>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
